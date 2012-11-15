31e31ed8a952dcba47d03d3339f91667ea05e3e3
Pei Yi <peiyiru@gmail.com>
2012-11-15 16:22:12 +0800

    loading screen, syncs used

--- views/js/sync.js
+++ views/js/sync.js
@@ -1,10 +1,26 @@
 function openingSync(){
-	console.log('shop has opened');
+	console.log('opening');
+		$.blockUI({ css: {
+            border: 'none',
+            padding: '15px',
+            backgroundColor: '#000',
+            '-webkit-border-radius': '10px',
+            '-moz-border-radius': '10px',
+            opacity: .5,
+            color: '#fff'
+        } });
+
+        //setTimeout($.unblockUI, 2000);
 	$.ajax({
 		url: "/syncAtStart",
 		type: 'GET',
 		success: function (response) {
+			$.unblockUI();
 			alert(response.responseText);
+		},
+		error: function (response) {
+			$.unblockUI();
+			alert('error..');
 		}
 	});
 }
